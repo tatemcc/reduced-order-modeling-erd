21b4643f0f39f7e93017289729ee74ac0546202d
Liger800000 <rishilandra@gmail.com>
2026-02-25 20:47:17 -0800

    just figured out the import thingy for snapshot given its in a cousin directory for movie_create

--- movie_studio/movie_create.py
+++ movie_studio/movie_create.py
@@ -1,5 +1,8 @@
 import numpy as np
 from pathlib import Path
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'burgers_rom', 'burgers_rom'))
+from snapshot import state_vec_to_fields, SnapshotLayout
 
 # Choose data files
 # Note: by default, stuff happens relative to where you run python, not where the script lives
@@ -20,7 +23,19 @@ def npy2arr(fp):
     return np.load(fp, allow_pickle=True)
 basis_U_arr_flat = npy2arr(basis_U_path)
 coeffs_A_arr = npy2arr(coeffs_A_path)
-mean_q_arr = npy2arr(mean_q_path)
-singular_values_arr_flat = npy2arr(singular_values_path)
+mean_q_arr_flat = npy2arr(mean_q_path)
+singular_values_arr = npy2arr(singular_values_path)
 
-print("kk")
+# Reinflate the spatial data (1D back to 3D)
+space_product = (basis_U_arr_flat.shape)[0]   # C*ny*nx
+nt = (coeffs_A_arr.shape)[1]
+total_num_modes = (singular_values_arr.shape)[0]
+assert(total_num_modes == min(space_product, nt))
+nsm = (coeffs_A_arr.shape)[0]   # number of selected modes (top nsm ranked by singular values)
+assert(nsm == (basis_U_arr_flat.shape)[1])
+# !!Caution: making assumptions here related to space_product, C, ny, and nx (hard-coding)
+C = 2
+ny = 15
+nx = 15
+assert(space_product == C*ny*nx)
+
